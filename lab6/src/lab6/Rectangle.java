7ef9abf88141b0f2fb94a0da3d7b99e495297d0a
jisungmon <jisungmon@gmail.com>
2016-04-27 17:23:59 +0900

    created class rectangle & shapetest

--- lab6/src/lab6/Rectangle.java
+++ lab6/src/lab6/Rectangle.java
@@ -1,20 +1,33 @@
 package lab6;
 
 public class Rectangle extends Shape{
-double width;
-double height;
 
-public Rectangle(double width, double height) {
-	
-	this.width = width;
-	this.height = height;
-}
-public double calculateArea(){
-	
-}
-public double calculatePerimeter();
-public void drawRectangle();
-void printInfo();
-
-
+   private double width;
+   private double height;
+   //constructor
+   public Rectangle(double width, double height) {
+      super();
+      this.width = width;
+      this.height = height;
+   }
+   
+   @Override
+   public double caculateArea() {
+      return width*height;
+   }
+   
+   @Override
+   public double caculatePerimeter() {
+      double perimeter=2*(height+width);
+      return perimeter;
+   }
+   @Override
+   public void printInfo() {
+      
+   }
+   
+   public void drawRectnagle(){
+	   
+   }
+   
 }
---
+++ lab6/src/lab6/ShapeTest.java
@@ -0,0 +1,24 @@
+package lab6;
+
+import java.util.ArrayList;
+
+public class ShapeTest {
+
+   public static void main(String[] args) {
+      ShapeTest shapetest = new ShapeTest();
+      shapetest.start();
+   }
+
+   public void start(){
+     
+      
+   }
+   
+   public void processShape(Shape s){
+    
+   }
+   
+   
+   
+   
+}
