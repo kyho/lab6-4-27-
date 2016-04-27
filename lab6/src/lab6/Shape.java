03a055054e01723406d3b89203511a4b7ca0da7a
KimYunHo <kyh@a.a>
2016-04-27 16:17:29 +0900

    add getters and setters

--- lab6/src/lab6/Shape.java
+++ lab6/src/lab6/Shape.java
@@ -3,5 +3,22 @@ package lab6;
 public abstract class Shape {
 	
 	private int id;
+	
+	public Shape()
+	{
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	public abstract double caculateArea();
+	
 
 }
