57ef78d79be8f701efe968413de048f5e81da251
jisungmon <jisungmon@gmail.com>
2016-04-27 16:55:26 +0900

    rectangle

--- lab6/src/lab6/Rectangle.java
+++ lab6/src/lab6/Rectangle.java
@@ -1,5 +1,20 @@
 package lab6;
 
-public class Rectangle {
+public class Rectangle extends Shape{
+double width;
+double height;
+
+public Rectangle(double width, double height) {
+	
+	this.width = width;
+	this.height = height;
+}
+public double calculateArea(){
+	
+}
+public double calculatePerimeter();
+public void drawRectangle();
+void printInfo();
+
 
 }
